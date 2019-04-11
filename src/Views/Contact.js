7b1787f33a668dfc1331f4d2578790e6e16ec716
Dallin-H <dallinhyde@gmail.com>
2019-04-11 10:52:39 -0600

    add project and contact. Upgrade styling

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
+import './Contact.css'
+
 export default class Contact extends Component {
   render() {
     return (
-      <div>
+      <div className="Contact">
+        <div className="Contact_Title">
         Contact
+        </div>
+        <div className="Contact_Body">
+          Please select an icon below to reach out! I'll be in touch with you soon.
+        </div>
       </div>
     )
   }
--- src/Views/Projects.js
+++ src/Views/Projects.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
+import './Projects.css'
+
 export default class Projects extends Component {
   render() {
     return (
-      <div>
-        Projects
+      <div className='Projects'>
+        <div className="Projects_Title">
+          My Recent Work
+        </div>
+        <div className="Projects_Container_Edudecks">
+          <strong className="Projects_Container_Edudecks_Title">
+            EduDecks
+          </strong>
+          <div className="Projects_Container_Edudecks_Body">
+            A flash card solution where users can create a deck of multiple choice questions. Made with the PERN stack.
+          </div>
+        </div>
+        <div className="Projects_Container_Frealxp">
+          <strong className="Projects_Container_Frealxp_Title">
+            FrealXP
+          </strong>
+          <div className="Projects_Container_Frealxp_Body">
+            FrealXP helps people to get out and enjoy the outdoors. Users accept challenges and earn rewards! Made with the Pern stack.
+          </div>
+        </div>
       </div>
     )
   }
